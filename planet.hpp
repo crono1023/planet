7d24e9850fb8b96d5961042b66b0aec9722538cb
crono1023 <crono1023@gmail.com>
2021-04-23 12:04:39 -0400

    initial commit

---
+++ planet.hpp
@@ -0,0 +1,19 @@
+#ifndef PLANET_HPP
+#define PLANET_HPP
+
+#include <string>
+
+using std::string;
+
+class Planet {
+    public:
+        Planet(string name, unsigned position);
+        void printDetails();
+
+    private:
+        string name;
+        int position;
+
+};
+
+#endif
